8835573a4200840e018be3593a436387a96698f7
mzacom <128300702+mzacom@users.noreply.github.com>
2023-04-07 04:01:53 -0700

    Add files via upload

---
+++ inner.js
@@ -0,0 +1,14 @@
+
+const welcome = "You're welcome to Gabriel Trading Academy"
+alert(welcome);
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "190px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
